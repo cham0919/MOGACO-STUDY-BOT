36c998c37885868aeef6ac4864a5ef2a93fd5671
ckadl0919 <ckadl0118@gmail.com>
2021-07-18 03:23:32 +0900

    refac. boot name 변경
    boot name 변경

--- src/main/java/com/studty/mogaco/boot/BootApplication.java
+++ src/main/java/com/studty/mogaco/boot/BootApplication.java
@@ -1,14 +1,14 @@
-package com.studty.mogaco.main;
+package com.studty.mogaco.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainApplication {
+public class BootApplication {
 
     
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+        SpringApplication.run(BootApplication.class, args);
     }
 
 }
--- src/test/java/com/studty/mogaco/boot/MainApplicationTests.java
+++ src/test/java/com/studty/mogaco/boot/MainApplicationTests.java
@@ -1,4 +1,4 @@
-package com.studty.mogaco.main;
+package com.studty.mogaco.boot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
