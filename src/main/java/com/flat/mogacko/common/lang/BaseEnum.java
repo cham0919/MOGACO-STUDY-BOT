c5f37804c233172ffc32c030088c808785b6b2c7
ckadl0919 <ckadl0118@gmail.com>
2021-07-18 16:53:34 +0900

    delete. project 초기화
    project 초기화

--- src/main/java/com/flat/mogacko/common/lang/BaseEnum.java
+++
@@ -1,9 +0,0 @@
-package com.flat.mogacko.common.lang;
-
-public interface BaseEnum {
-
-    boolean equalsValue(String value);
-
-    boolean equalsIgnoreValue(String value);
-
-}
--- src/main/java/com/flat/mogacko/env/Environment.java
+++
@@ -1,9 +0,0 @@
-package com.flat.mogacko.env;
-
-import java.util.Properties;
-
-public interface Environment {
-
-    Properties getAllProperties();
-
-}
