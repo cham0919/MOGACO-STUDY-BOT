f3a3640522a5a37312242aa08048e790a100951e
ckadl0919 <ckadl0118@gmail.com>
2021-07-20 20:08:33 +0900

    feat. 메세지 관리 변환기 수정
    메세지 관리 변환기 수정

--- src/main/java/com/flat/mogacko/bot/discord/BehaviorTransponder.java
+++ src/main/java/com/flat/mogacko/bot/discord/BehaviorTransponder.java
@@ -1,6 +1,8 @@
 package com.flat.mogacko.bot.discord;
 
+import com.flat.mogacko.Join.JoinRecordService;
 import com.flat.mogacko.env.Config;
+import com.flat.mogacko.member.MemberDto;
 import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
@@ -14,6 +16,9 @@ import javax.annotation.Nonnull;
 @AllArgsConstructor
 public class BehaviorTransponder extends Transponder {
 
+    private final JoinRecordService joinRecordService;
+
+
     @Override
     public void onRoleCreate(@NotNull RoleCreateEvent event) {
         // 공지사항 메세지
@@ -23,10 +28,18 @@ public class BehaviorTransponder extends Transponder {
     @Override
     public void onGuildVoiceJoin(@Nonnull GuildVoiceJoinEvent event) {
         String nickName = event.getMember().getEffectiveName();
+        String channel = event.getEntity().getGuild().getName();
+        MemberDto memberDto = new MemberDto().setNickName(nickName)
+                .setChannel(channel);
+        joinRecordService.voiceJoinStart(memberDto);
     } // 보이스 참여
 
     @Override
     public void onGuildVoiceLeave(@Nonnull GuildVoiceLeaveEvent event) {
         String nickName = event.getMember().getEffectiveName();
+        String channel = event.getEntity().getGuild().getName();
+        MemberDto memberDto = new MemberDto().setNickName(nickName)
+                .setChannel(channel);
+        joinRecordService.voiceJoinLeave(memberDto);
     } // 보이스 종료
 }
--- src/main/java/com/flat/mogacko/bot/discord/MessageTransponder.java
+++ src/main/java/com/flat/mogacko/bot/discord/MessageTransponder.java
@@ -1,9 +1,6 @@
 package com.flat.mogacko.bot.discord;
 
-import com.flat.mogacko.bot.discord.message.MessageService;
-import com.flat.mogacko.command.Command;
 import com.flat.mogacko.command.CommandHandler;
-import com.flat.mogacko.env.Config;
 import lombok.AllArgsConstructor;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.springframework.stereotype.Service;
@@ -13,28 +10,16 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class MessageTransponder extends Transponder{
 
-    private final MessageService messageService;
     private final CommandHandler commandHandler;
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event){
         String message = event.getMessage().getContentRaw();
         if (message.startsWith("!")) {
-            message = message.substring(1);
-            Command command = Command.findByMessage(message);
-            String respMessage = (String)commandHandler.commandHandle(null, message, command);
+            String respMessage = (String)commandHandler.commandHandle(event, message.substring(1).trim());
             if (respMessage != null) {
                 event.getChannel().sendMessage(respMessage).queue();
             }
-        }
-    }
-
-    public String fetchResponseMessage(String message) {
-        String respMessage = Config.getProperty(message.substring(1));
-        if (respMessage != null) {
-            return respMessage;
-        } else {
-            return messageService.getResponse(message);
         }
     }
 }
