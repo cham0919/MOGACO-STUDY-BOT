e322c1f33e7eff81756c3c0354283ffe26f47ceb
ckadl0919 <ckadl0118@gmail.com>
2021-07-20 20:08:48 +0900

    feat. Command 명령어, 옵션 분리 기능 추가
    Command 명령어, 옵션 분리 기능 추가

--- src/main/java/com/flat/mogacko/command/CommandHandler.java
+++ src/main/java/com/flat/mogacko/command/CommandHandler.java
@@ -4,6 +4,7 @@ import com.flat.mogacko.annot.CommandMapping;
 import com.flat.mogacko.bot.discord.Transponder;
 import com.flat.mogacko.common.util.BeanUtils;
 import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.reflections.Reflections;
 import org.reflections.scanners.MethodAnnotationsScanner;
 import org.reflections.util.ClasspathHelper;
@@ -14,15 +15,14 @@ import javax.annotation.PostConstruct;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 @Service
 public class CommandHandler {
 
-    private Map<Command, Method> commandMethod = new ConcurrentHashMap<>();
+    private Map<String, Map<String, Method>> commandMethod = new ConcurrentHashMap<>();
 
     @PostConstruct
     public void loadMogacoMessage(){
@@ -36,39 +36,73 @@ public class CommandHandler {
         try {
             methods.forEach(method -> {
                 CommandMapping commandMappingAnnotation = method.getAnnotation(CommandMapping.class);
-                Command key = commandMappingAnnotation.command();
-                commandMethod.put(key, method);
+                String command = commandMappingAnnotation.command();
+                String option = "None";
+                if (command.contains(" ")) {
+                    String[] arrayStr = command.split(" ");
+                    command = arrayStr[0];
+                    option = arrayStr[1];
+                }
+                Map<String, Method> optionMap = commandMethod.getOrDefault(command, new ConcurrentHashMap<>());
+                optionMap.put(option, method);
+                commandMethod.put(command, optionMap);
             });
         }  catch (Throwable t) {
             log.error(t.getMessage(), t);
         }
     }
 
-    public Object commandHandle(Map<String, String> chatRoomInfo, String text, Command command){
+    public Object commandHandle(MessageReceivedEvent event, String message){
+
+        Method method = null;
+        String command;
+        String option;
+        String param = null;
+        if (message.contains(" ")) {
+            command = message.substring(0, message.indexOf(" ")).trim();
+            message = message.substring(command.length()).trim();
+        } else {
+            command = message.trim();
+            message = message.substring(command.length()).trim();
+        }
 
         // Command 가 실행할 메서드
-        Method method = commandMethod.get(command);
+        Map<String, Method> optionMap = commandMethod.get(command);
+
+        if (message.contains(" ")) {
+            option = message.substring(0, message.indexOf(" ")).trim();
+        } else {
+            option = message;
+        }
+
+        if (optionMap.containsKey(option)) {
+            method = optionMap.get(option);
+            param = message.substring(option.length()).trim();
+        } else {
+            method = optionMap.get("None");
+            param = message.length() == 0 ? null : message;
+        }
 
         // 실행시킬 메소드가 있는 Controller
         Object controller = BeanUtils.getBean(method.getDeclaringClass());
 
         // 실행시킬 메소드의 파라미터
-        Object[] params = getParams(chatRoomInfo, text, method);
+        Object[] params = getParams(event, param, method);
 
-        Object message = null;
+        Object respMessage = null;
 
         try {
-            message = method.invoke(controller, params);
+            respMessage = method.invoke(controller, params);
         } catch (IllegalAccessException e) {
             log.error(e.getMessage(), e);
         } catch (InvocationTargetException e) {
             log.error(e.getMessage(), e);
         }
-        return message;
+        return respMessage;
     }
 
     // 메소드 파라미터 준비
-    private Object[] getParams(Map<String, String> chatRoomInfo, String text, Method method) {
+    private Object[] getParams(MessageReceivedEvent event, String param, Method method) {
 
         //
         final Parameter[] parameters = method.getParameters();
@@ -76,15 +110,15 @@ public class CommandHandler {
         final int parameterCount = method.getParameterCount();
 
         Object[] params = new Object[parameterCount];
-
         for (int i = 0; i < parameterCount; i++) {
             Class classOfParam = parameterTypes[i];
             String nameOfParam = parameters[i].getName();
 
-            if (classOfParam == String.class && nameOfParam.equals("text")) {
-                params[i] = text;
+            if (classOfParam == String.class && nameOfParam.equals("param") && param != null) {
+                params[i] = param;
+            } else if (classOfParam == MessageReceivedEvent.class) {
+                params[i] = event;
             }
-
         }
         return params;
     }
