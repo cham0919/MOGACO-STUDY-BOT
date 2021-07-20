e36dbe0958b949ac8541ca5479caec37b4877162
ckadl0919 <ckadl0118@gmail.com>
2021-07-20 20:07:44 +0900

    feat. 일반 Message 기능 추가
    일반 Message 기능 추가

--- src/main/java/com/flat/mogacko/Message/MessageController.java
+++ src/main/java/com/flat/mogacko/Message/MessageController.java
@@ -1,20 +1,27 @@
-package com.flat.mogacko.bot.discord.message;
+package com.flat.mogacko.Message;
 
-import com.flat.mogacko.Join.JoinService;
-import lombok.AllArgsConstructor;
+import com.flat.mogacko.annot.CommandMapping;
+import com.flat.mogacko.env.Config;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
-@AllArgsConstructor
-public class MessageService {
+public class MessageController {
 
-    private final JoinService joinService;
-
-    public String getResponse(String message) {
-        if (message.startsWith("참여")) {
-            return "참여";
+    @CommandMapping(command = "공지")
+    public String getRole(String param){
+        if (param == null) {
+            return Config.getProperty("공지");
         } else {
-            return null;
+            Config.setProperty("공지", param);
+            return "공지가 등록되었습니다.";
         }
+    }
+
+    @CommandMapping(command = "공지 삭제")
+    public String deleteRole(){
+        Config.setProperty("공지", "등록된 공지가 없습니다 :(");
+        return "공지가 삭제되었습니다";
     }
 }
