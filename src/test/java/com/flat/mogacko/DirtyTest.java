9a47e5a595f63509b3ad6ecfc5d127da4adaaccc
ckadl0919 <ckadl0118@gmail.com>
2021-07-21 22:30:22 +0900

    test. DirtyTest 추가
    DirtyTest 추가

---
+++ src/test/java/com/flat/mogacko/DirtyTest.java
@@ -0,0 +1,27 @@
+package com.flat.mogacko;
+
+import com.flat.mogacko.rank.Rank;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class DirtyTest {
+
+    @Test
+    public void test() throws InterruptedException {
+        List<Rank> rankList = new ArrayList<>();
+        LocalTime now = LocalTime.now();
+        rankList.add(new Rank().setTotaljointime(now.plusHours(1)).setKey(3L));
+        rankList.add(new Rank().setTotaljointime(now.plusMinutes(1)).setKey(2L));
+        rankList.add(new Rank().setTotaljointime(now).setKey(1L));
+
+        rankList.stream()
+                .sorted(Comparator.comparing(Rank::getTotaljointime));
+
+    }
+}
