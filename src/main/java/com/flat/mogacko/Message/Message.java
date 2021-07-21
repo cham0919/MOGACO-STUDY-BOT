dbb3f3473c8d22ee72f43f0a8596682fa8052cb0
ckadl0919 <ckadl0118@gmail.com>
2021-07-21 22:30:37 +0900

    refac. error Message 수정
    error Message 수정

---
+++ src/main/java/com/flat/mogacko/Message/Message.java
@@ -0,0 +1,10 @@
+package com.flat.mogacko.Message;
+
+
+import lombok.AllArgsConstructor;
+
+public class Message {
+
+
+    public static final String ERROR = "웁스! 에러 발생 ㅜㅜ";
+}
--- src/main/java/com/flat/mogacko/rank/RankController.java
+++ src/main/java/com/flat/mogacko/rank/RankController.java
@@ -1,16 +1,13 @@
 package com.flat.mogacko.rank;
 
 
+import com.flat.mogacko.Message.Message;
 import com.flat.mogacko.annot.CommandMapping;
-import com.flat.mogacko.member.MemberDto;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalTime;
-import java.util.List;
 
 @Slf4j
 @AllArgsConstructor
@@ -24,7 +21,7 @@ public class RankController {
         try {
             return rankService.fetchCurrentRankMessage(event.getGuild().getName());
         }catch (DataIntegrityViolationException e) {
-            return "웁스! 에러 발생 ㅜㅜ";
+            return Message.ERROR;
         }
     }
 }
