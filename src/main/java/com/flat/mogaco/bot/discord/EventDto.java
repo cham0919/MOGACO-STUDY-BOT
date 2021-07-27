f5857adeef5945a0eda778e0fc782316873398d7
ckadl0919 <ckadl0118@gmail.com>
2021-07-28 08:14:41 +0900

    feat. EventDto 추가
    EventDto 추가

---
+++ src/main/java/com/flat/mogaco/bot/discord/EventDto.java
@@ -0,0 +1,21 @@
+package com.flat.mogaco.bot.discord;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+@Getter @Setter
+@Accessors(chain = true)
+public class EventDto {
+
+    private String channelId;
+    private String channelName;
+    private String nickName;
+
+    public EventDto(MessageReceivedEvent event) {
+        this.channelId = event.getGuild().getId();
+        this.channelName = event.getGuild().getName();
+        this.nickName = event.getMember().getEffectiveName();
+    }
+}
